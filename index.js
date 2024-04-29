232484d94b818585451c2ec90499c101c6cbfd6c
sususaiasusubendera <oplessnewbee@gmail.com>
2024-04-29 21:35:28 +0700

    PASS alternative 2 (simple ver)

--- index.js
+++ index.js
@@ -19,9 +19,37 @@ app.get("/", function (req, res) {
 });
 
 
+
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
+});
+
+// tests
+// alternative 2
+const isInvalidDate = (date) => date.toUTCString() === "Invalid Date";
+
+app.get("/api/:date", (req, res) => {
+  let date = new Date(req.params.date);
+  
+  if (isInvalidDate(date)) date = new Date(+req.params.date);
+
+  if (isInvalidDate(date)) {
+    res.json({ error: "Invalid Date" });
+    return;
+  }
+
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  });
+});
+
+app.get("/api", (req, res) => {
+  res.json({
+    unix: new Date().getTime(),
+    utc: new Date().toUTCString()
+  });
 });
 
 
